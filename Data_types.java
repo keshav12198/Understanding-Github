a1b516ff1b06eb3536d0cfec85c08f158399da6d
Keshav <keshav12198@gmail.com>
2019-06-25 17:05:03 +0530

    Change commited

---
+++ Data_types.java
@@ -0,0 +1,17 @@
+public class Data_types
+{
+	int i;
+	float f=32.0f;
+	long l;
+	double d=34;
+	byte b;
+	short s;
+	char c;
+	boolean boo;
+	String st;
+	public static void main(String args[])
+	{
+		Data_types data=new Data_types();
+		System.out.println(data.i+" "+data.f+" "+data.l+" "+data.d+" "+data.b+" "+data.s+" "+data.c+" "+data.boo+" "+data.st+" "+data);
+	}
+}
